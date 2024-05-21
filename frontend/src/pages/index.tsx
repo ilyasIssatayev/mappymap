8dbb7491c6191738b6e8e6ca50afc9982bb50c04
ilyas issatayev <issatayev.ilyas@gmail.com>
2024-05-21 20:49:01 +0200

    Add first basic connection

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,49 @@
-function Home() {
+import { useState, useEffect } from 'react';
+
+function LandingPage() {
+
+    const query = `[out:json];
+    area[name="Enschede"]->.searchArea;
+    (
+      //node["building"](area.searchArea);
+      way["building"](area.searchArea);
+      //relation["building"](area.searchArea);
+    );
+    out body;
+    >;
+    out skel qt;`;
+
+// Encode the query
+const encodedQuery = encodeURIComponent(query);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log("ping")
+          try {
+            const apiUrl = `https://overpass-api.de/api/interpreter?data=${encodedQuery}`;
+            const response = await fetch(apiUrl);
+            if (!response.ok) {
+              throw new Error('Failed to fetch data');
+            }
+            const jsonData = await response.json();
+            console.log(jsonData)
+
+          } catch (error) {
+            console.log('error',error)
+
+          } finally {
+
+          }
+        };
+    
+        fetchData();
+      }, []);
+
+
+
     return (
-        <div>mappy map</div>
+        <div>mappymap</div>
     );
 }
 
-export default Home;
+export default LandingPage
